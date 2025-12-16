de738ddb18fbf34c1ca180ef019322dec3cedb50
DonSteve777 <93adones@gmail.com>
2025-12-16 13:20:59 +0100

    clase Applicaiton

---
+++ backend/src/test/java/com/trainhub/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
