2bb3dc42e6ae11576e345fe49113e71cde29ac55
DonSteve777 <93adones@gmail.com>
2026-03-07 19:14:13 +0100

    pantalla aviso email de verificacion

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
 import { ConfirmEmailComponent } from './features/confirm-email/confirm-email.component';
 import { UserProfileComponent } from './features/user-profile/user-profile.component';
-import { RegisterComponent } from './features/register/register.component';
+import { VerifyEmailComponent } from './features/verify-email/verify-email.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'confirm-email/:token', component: ConfirmEmailComponent },
   { path: 'user-profile', component: UserProfileComponent },
-   { path: 'register', component: RegisterComponent },
+  { path: 'verify-email', component: VerifyEmailComponent },
   { path: '**', redirectTo: '' },
 ];
